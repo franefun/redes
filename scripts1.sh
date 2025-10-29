522320f01b65deca84f53efe59e69f3a1698ea51
franefun <franefun@gmail.com>
2025-10-29 19:54:10 +0100

    vpc y subred listas

--- scripts1.sh
+++ scripts1.sh
@@ -11,3 +11,18 @@ VPC_ID=$(aws ec2 create-vpc \
     aws ec2 modify-vpc-attribute \
     --vpc-id $VPC_ID \
     --enable-dns-hostnames "{\"Value\":true}"
+
+    #Crear una subnet
+    SUB_ID=$(aws ec2 create-subnet \
+    --vpc-id $VPC_ID \
+    --cidr-block 192.168.0.0/28 \
+    --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Mi-Subred1.fran}]' \
+    --query 'Subnet.SubnetId' \
+    --output text)
+
+    #Muestro la id de la subnet
+    echo $SUB_ID
+
+    #Habilita la asignación de la ipv4publica en la subred
+    #Comprobar como NO se habilita y tenemos que hacerlo a posteriori
+    aws ec2 modify-subnet-attribute --subnet-id $SUB_ID --map-public-ip-on-launch
